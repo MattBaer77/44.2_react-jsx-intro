db8cd4ae7cd097127e7536b0252de2bedb4f304d
Matthew Baer <matt.baer77@gmail.com>
2023-08-21 22:33:13 -0400

    Part 2 Working

---
+++ part-2/App.js
@@ -0,0 +1,18 @@
+// const tweets = [
+//                 {
+//                     username : "User 1",
+//                     name : "User 1",
+//                     date : "User 1",
+//                     message : "User 1"
+//                 }
+//             ]
+
+const App = () => (
+    <div>
+        <Tweet username="User 1" name="Name 1" date="2023-08-21" message="I am the first tweeter to tweet!" />
+        <Tweet username="User 2" name="Name 2" date="2023-08-22" message="I am the second tweeter to tweet!" />
+        <Tweet username="User 3" name="Name 3" date="2023-08-23" message="I am the third tweeter to tweet!" />
+    </div>
+)
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ part-2/Tweet.js
@@ -0,0 +1,14 @@
+const Tweet = ({username, name, date, message}) => (
+
+    <div>
+
+        <h2>{username}</h2>
+        <h3>{name}</h3>
+        <h3>{date}</h3>
+        <p>{message}</p>
+
+        <br />
+
+    </div>
+
+)
